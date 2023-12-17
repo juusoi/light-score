7ba6f1f4bcf6855e711932d9a2c1116018cc4169
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-12-17 18:48:08 +0200

    fronttia

--- frontend/src/utest/test_app.py
+++ frontend/src/utest/test_app.py
@@ -1,18 +1,18 @@
 import pytest
+from app import app
 from flask_testing import TestCase
 
-from app import app
 
 class MyTest(TestCase):
-
     def create_app(self):
-        app.config['TESTING'] = True
+        app.config["TESTING"] = True
         return app
 
     def test_home_route(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Welcome to NFL Scores and Standings', response.data)
+        self.assertIn(b"Welcome to NFL Scores and Standings", response.data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pytest.main()
