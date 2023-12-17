ac4fcabce936e681410414beec5ea524d9a3825a
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-12-17 19:03:30 +0200

    front with local be

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,11 +1,22 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BACKEND_URL = "http://localhost:8000"
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    games_response = requests.get(f"{BACKEND_URL}/games")
+    standings_response = requests.get(f"{BACKEND_URL}/standings")
+
+    if games_response.ok and standings_response.ok:
+        games_data = games_response.json()
+        standings_data = standings_response.json()
+        return render_template("home.html", games=games_data, standings=standings_data)
+    else:
+        return render_template("home_local.html")
 
 
 def main():
