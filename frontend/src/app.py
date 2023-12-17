48b0cda461b1723d28afd69db92b2840504a977d
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-12-17 19:12:19 +0200

    fix no api

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -8,15 +8,18 @@ BACKEND_URL = "http://localhost:8000"
 
 @app.route("/")
 def home():
-    games_response = requests.get(f"{BACKEND_URL}/games")
-    standings_response = requests.get(f"{BACKEND_URL}/standings")
+    try:
+        games_response = requests.get(f"{BACKEND_URL}/games")
+        standings_response = requests.get(f"{BACKEND_URL}/standings")
+    except requests.exceptions.ConnectionError:
+        return render_template("home_no_api.html")
 
     if games_response.ok and standings_response.ok:
         games_data = games_response.json()
         standings_data = standings_response.json()
         return render_template("home.html", games=games_data, standings=standings_data)
     else:
-        return render_template("home_local.html")
+        return "Error: Could not retrieve data from backend.", 500
 
 
 def main():
